e2fea74947b2dfe516dee3c31cb89bb3dd794e1c
좌 준협 <>
2018-06-29 00:32:26 +0900

    mapping java object with table, and made sign up interface

--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -1,10 +1,28 @@
-package com.example.demo.webs;
+package com.example.demo.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue
+	private long key;
+	
+	
+	@Column(nullable = false)
 	private String id;
+	@Column(nullable = false)
 	private String name;
+	@Column(nullable = false)
 	private String email;
+	@Column(nullable = false)
 	private String password;
+	
 	public String getId() {
 		return id;
 	}
--- src/main/java/com/example/demo/webs/UserController.java
+++ src/main/java/com/example/demo/webs/UserController.java
@@ -3,25 +3,33 @@ package com.example.demo.webs;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.domain.User;
+import com.example.demo.domain.UserRepository;
 
 @Controller
 public class UserController {
 	
 	private List<User> users = new ArrayList<User>();
 	
+	@Autowired
+	UserRepository userRepository;
+	
 	@PostMapping("/create")
 	public String create(User user) {
-		users.add(user);
+		//users.add(user);
+		userRepository.save(user);
 		return "redirect:/list";
 	}
 	
 	@GetMapping("/list")
 	public String list(Model model) {
-		model.addAttribute("users", users);
+		model.addAttribute("users", userRepository.findAll());
 		return "UserList/index";
 	}
 }
