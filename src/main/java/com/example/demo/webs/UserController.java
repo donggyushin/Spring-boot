27419620331bf86b285c99847c6a13e359efc68e
좌 준협 <>
2018-06-29 18:31:45 +0900

    Edit profile part1

--- src/main/java/com/example/demo/webs/UserController.java
+++ src/main/java/com/example/demo/webs/UserController.java
@@ -2,11 +2,13 @@ package com.example.demo.webs;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.domain.User;
@@ -20,6 +22,17 @@ public class UserController {
 	@Autowired
 	UserRepository userRepository;
 	
+	@GetMapping("/SignIn")
+	public String SignIn() {
+		return "SignIn/index";
+	}
+	
+	@GetMapping("/SignUp")
+	public String SignUp() {
+		return "SignUp/index";
+	}
+	
+	
 	@PostMapping("/create")
 	public String create(User user) {
 		//users.add(user);
@@ -31,5 +44,12 @@ public class UserController {
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
 		return "UserList/index";
+	}
+	
+	@GetMapping("/update/{id}")
+	public String update(@PathVariable("id") Long id,Model model) {
+		User user = userRepository.findById(id).get();
+		model.addAttribute("user", user);
+		return "Update/index";
 	}
 }
