7a3c3481f8e7820d00d21425b1ceac7638a03d43
좌 준협 <>
2018-07-02 13:06:21 +0900

    Question page implement

--- src/main/java/com/example/demo/webs/QuestionController.java
+++ src/main/java/com/example/demo/webs/QuestionController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.domain.Question;
@@ -33,13 +34,21 @@ public class QuestionController {
 		
 		Question newQuestion = new Question();
 		
-		String questionsWriter = writer.getName();
+		String questionsWriter = writer.getId();
 		newQuestion.setContents(contents);
 		newQuestion.setTitle(title);
 		newQuestion.setWriter(questionsWriter);
 		questionRepository.save(newQuestion);
 		
 		return "redirect:/";
+	}
+	
+	@GetMapping("/question/{id}")
+	public String Question(@PathVariable long id, Model model) {
+		
+		Question question = questionRepository.findById(id).get();
+		model.addAttribute("question", question);
+		return "/Question/index";
 	}
 	
 	
